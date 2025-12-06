c0fd879a29e84716b6c34235605ee0cdf1dd62f5
GedIra <irankundag65@gmail.com>
2025-12-07 00:01:26 +0200

    fix: Front end errors

--- Abasare/settings.py
+++ Abasare/settings.py
@@ -67,6 +67,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            
+            'builtins': [
+                "lucide.templatetags.lucide",
+            ],
         },
     },
 ]
